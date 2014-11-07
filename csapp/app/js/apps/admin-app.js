4a9fe60df2f3009eecd1d5d394f62bfaf9a4d3d7
Bojan Mojsilovic <bmojsilovic@itekako.com>
2014-11-07 11:46:22 +0100

    Add check for undefined or empty string and array in 'GroupMembers'.
    Check if the provided string or array is not undefined or empty
    when converting between these two types.

--- csapp/app/js/apps/admin-app.js
+++ csapp/app/js/apps/admin-app.js
@@ -640,6 +640,9 @@ App.GroupMembers = Ember.Object.extend({});
 
 App.GroupMembers.reopenClass({
 	toArrayFromString: function(string){
+		if(string === undefined || string.length === 0){
+			return [];
+		}
 		var recipient_names = string.split(",");
 		return recipient_names.map(function(name){
 			return name.trim();
@@ -647,6 +650,9 @@ App.GroupMembers.reopenClass({
 	},
 
 	toStringFromArray: function(memberArray){
+		if(memberArray === undefined || memberArray.length === 0){
+			return "";
+		}
 		return memberArray.map(function(member) {
 			return member.name;
 		}).join(", ");
