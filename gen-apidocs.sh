822686379f673b3e31b8855d22245934292b4f9f
jonathan <jonathan@kyuss.org>
2014-11-20 08:12:47 -0800

    fix API doc generation

--- gen-apidocs.sh
+++ gen-apidocs.sh
@@ -10,10 +10,10 @@ mkdir -p ${SRC_DIR}
 rsync -avzc ${BASE_DIR}/src/main/* ${SRC_DIR}
 rsync -avzc ${BASE_DIR}/../cloudos-lib/src/main/* ${SRC_DIR}
 rsync -avzc ${BASE_DIR}/../cloudos-appstore/appstore-common/src/main/* ${SRC_DIR}
-rsync -avzc ${BASE_DIR}/../utils/cobbzilla-utils/src/main ${SRC_DIR}
-rsync -avzc ${BASE_DIR}/../utils/cobbzilla-wizard/wizard-server/src/main ${SRC_DIR}
-rsync -avzc ${BASE_DIR}/../utils/rooty-tooty/src/main ${SRC_DIR}
-rsync -avzc ${BASE_DIR}/../utils/rooty-toots/src/main ${SRC_DIR}
+rsync -avzc ${BASE_DIR}/../utils/cobbzilla-utils/src/main/* ${SRC_DIR}
+rsync -avzc ${BASE_DIR}/../utils/cobbzilla-wizard/wizard-server/src/main/* ${SRC_DIR}
+rsync -avzc ${BASE_DIR}/../utils/rooty-tooty/src/main/* ${SRC_DIR}
+rsync -avzc ${BASE_DIR}/../utils/rooty-toots/src/main/* ${SRC_DIR}
 
 cp pom-apidocs.xml ${BUILD_DIR}/pom.xml
 
