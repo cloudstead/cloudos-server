774e13bfc599721cd55b736c3df0d8b4b48a9f7a
jonathan <jonathan@kyuss.org>
2014-11-19 18:15:22 -0800

    adjust run-list depending on DNS strategy

--- chef-repo/deploy.sh
+++ chef-repo/deploy.sh
@@ -45,4 +45,24 @@ ${CLOUDOS_BASE}/cloudos-lib/chef-repo/cookbooks \
 $(find ${CLOUDOS_BASE}/cloudos-apps/apps -type d -name cookbooks) \
 "
 
-${DEPLOYER} ${host} ${INIT_FILES} "${REQUIRED}" "${COOKBOOK_SOURCES}"
+SOLO_JSON="$(pwd)/solo-base.json"
+TMP_SOLO=$(mktemp /tmp/solo-json.XXXXXX) || die "Error creating temp file for solo.json"
+
+# If not using Dyn directly from cloudos, install cloudos-dns
+JSON_EDIT="java -cp ${BASE}/../target/cloudos-server-*.jar org.cobbzilla.util.json.main.JsonEditor"
+DYN_ZONE=$(${JSON_EDIT} -f data_bags/cloudos/init.json -o read -p dns.zone | tr -d ' ')
+if [ -z ${DYN_ZONE} ] ; then
+  REQUIRED="${REQUIRED} \
+    data_bags/cloudos-dns/init.json \
+    data_bags/cloudos-dns/ports.json"
+
+  # Add cloudos-dns recipe
+  ${JSON_EDIT} -f ${SOLO_JSON} -o write -p run_list[] -v \"recipe[cloudos-dns]\" > ${TMP_SOLO}
+else
+  cat ${SOLO_JSON} > ${TMP_SOLO}
+fi
+
+# Add cloudos recipe
+${JSON_EDIT} -f ${TMP_SOLO} -o write -p run_list[] -v \"recipe[cloudos]\" > ${TMP_SOLO}
+
+${DEPLOYER} ${host} ${INIT_FILES} "${REQUIRED}" "${COOKBOOK_SOURCES}" ${TMP_SOLO}
