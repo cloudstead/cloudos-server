65cbe079dea15c37e2b0b1046d095887b2e2bce7
jonathan <jonathan@kyuss.org>
2015-02-11 12:58:49 -0800

    ensure chef files are at least group-readable so that chef-user can read them

--- src/main/java/cloudos/service/AppInstallTask.java
+++ src/main/java/cloudos/service/AppInstallTask.java
@@ -143,7 +143,7 @@ public class AppInstallTask extends TaskBase {
             if (configuration.getRootyGroup() != null) {
                 CommandShell.chgrp(configuration.getRootyGroup(), configuration.getAppRepository(), true);
             }
-            CommandShell.chmod(configuration.getAppRepository(), "750", true);
+            CommandShell.chmod(configuration.getAppRepository(), "g+r", true);
         } catch (Exception e) {
             error("{appInstall.error.perms", "Error setting ownership/permissions on "+configuration.getAppRepository().getAbsolutePath()+": "+e);
             return null;
