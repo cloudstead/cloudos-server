ec6967a37babeb8cb0a3bebab6984ad8bcbe204c
Bojan Mojsilovic <bmojsilovic@itekako.com>
2015-02-18 10:03:54 +0100

    Show app name in header if no alt text is provided.

--- csapp/app/js/apps/admin-app.js
+++ csapp/app/js/apps/admin-app.js
@@ -1318,6 +1318,11 @@ App.EappController = Ember.ObjectController.extend({
 		var assets = this.get('assets');
 		return !Ember.isNone(assets) &&
 			(!Ember.isNone(assets.taskbarIconUrl) || !Ember.isEmpty(assets.taskbarIconUrl));
+	}.property(),
+
+	taskbarIconCaption: function(){
+		var text = this.get('assets.taskbarIconAltText');
+		return text === undefined ? this.get('name') : text;
 	}.property()
 });
 
