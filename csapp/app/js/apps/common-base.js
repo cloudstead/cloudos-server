ad59c26d147ae13379a825a1e00414813bb39cf1
Bojan Mojsilovic <bmojsilovic@itekako.com>
2015-03-09 15:18:39 +0100

    Split 'common.js' into multiple files.
    These files are in the 'common' folder.

---
+++ csapp/app/js/apps/common-base.js
@@ -0,0 +1,32 @@
+String.prototype.trim = String.prototype.trim || function trim() { return this.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); };
+
+function initialize_zurb() {
+	Ember.$(document).foundation();
+}
+
+function locate(obj, path) {
+	if (!path) return null;
+	if (path[0] == '{' && path[path.length-1] == '}') {
+		// strip leading/trailing curlies, if present
+		path = path.substring(1, path.length-1);
+	}
+	path = path.split('.');
+	var arrayPattern = /(.+)\[(\d+)\]/;
+	for (var i = 0; i < path.length; i++) {
+		var match = arrayPattern.exec(path[i]);
+		if (match) {
+			obj = obj[match[1]][parseInt(match[2])];
+		} else {
+			obj = obj[path[i]];
+		}
+	}
+
+	return obj;
+}
+
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
---
+++ csapp/app/js/apps/common/devices.js
@@ -0,0 +1,13 @@
+function generateDeviceId()
+{
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for( var i=0; i < 20; i++ ){
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function getDeviceName(){
+	return navigator.userAgent;
+}
