5a5eb2503ab21fd5fd12101205c19f180c57c3e8
jonathan <jonathan@kyuss.org>
2015-05-24 14:27:35 -0700

    refactor, use ResourceUtil.streamFile

--- src/main/java/cloudos/resources/AppAssetsResource.java
+++ src/main/java/cloudos/resources/AppAssetsResource.java
@@ -3,20 +3,15 @@ package cloudos.resources;
 import cloudos.appstore.model.app.AppLayout;
 import cloudos.server.CloudOsConfiguration;
 import com.qmino.miredot.annotations.ReturnType;
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.cobbzilla.wizard.resources.ResourceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
-import java.io.*;
-import java.net.URLConnection;
+import java.io.File;
 
 @Consumes(MediaType.WILDCARD)
 @Produces(MediaType.WILDCARD)
@@ -45,15 +40,7 @@ public class AppAssetsResource {
         final File assetFile = layout.findDefaultAsset(asset);
         if (assetFile == null || !assetFile.exists()) return ResourceUtil.notFound(asset);
 
-        return Response.ok(new StreamingOutput() {
-            @Override public void write(OutputStream out) throws IOException, WebApplicationException {
-                @Cleanup final InputStream in = new FileInputStream(assetFile);
-                IOUtils.copy(in, out);
-            }
-        })
-                .header(HttpHeaders.CONTENT_TYPE, URLConnection.guessContentTypeFromName(assetFile.getName()))
-                .header(HttpHeaders.CONTENT_LENGTH, assetFile.length())
-                .build();
+        return ResourceUtil.streamFile(assetFile);
     }
 
 }
