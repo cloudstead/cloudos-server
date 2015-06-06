08e63aba37826a5870c5e4991309c9069e66bc2e
jonathan <jonathan@kyuss.org>
2015-06-05 18:53:42 -0700

    add locale and appname args

--- src/main/java/cloudos/main/app/SearchAppStoreOptions.java
+++ src/main/java/cloudos/main/app/SearchAppStoreOptions.java
@@ -11,7 +11,7 @@ import org.kohsuke.args4j.Option;
 public class SearchAppStoreOptions extends PagedCloudOsMainOptions {
 
     public static final String USAGE_LEVEL = "Only show apps at this level. Default is 'app'";
-    public static final String OPT_LEVEL = "-L";
+    public static final String OPT_LEVEL = "-l";
     public static final String LONGOPT_LEVEL = "--level";
     @Option(name=OPT_LEVEL, aliases=LONGOPT_LEVEL, usage=USAGE_LEVEL)
     @Getter @Setter private AppLevel level = AppLevel.app;
@@ -22,7 +22,23 @@ public class SearchAppStoreOptions extends PagedCloudOsMainOptions {
     @Option(name=OPT_TYPE, aliases=LONGOPT_TYPE, usage=USAGE_TYPE)
     @Getter @Setter private AppStoreObjectType type;
 
+    public static final String USAGE_LOCALE = "Use this locale";
+    public static final String OPT_LOCALE = "-L";
+    public static final String LONGOPT_LOCALE = "--locale";
+    @Option(name=OPT_LOCALE, aliases=LONGOPT_LOCALE, usage=USAGE_LOCALE)
+    @Getter @Setter private String locale = null;
+
+    public static final String USAGE_APPNAME = "Find only this app";
+    public static final String OPT_APPNAME = "-N";
+    public static final String LONGOPT_APPNAME = "--app-name";
+    @Option(name=OPT_APPNAME, aliases=LONGOPT_APPNAME, usage=USAGE_APPNAME)
+    @Getter @Setter private String appName = null;
+
     public AppStoreQuery getQuery () {
-        return new AppStoreQuery(getPage()).setLevel(level).setType(type);
+        return new AppStoreQuery(getPage())
+                .setAppName(appName)
+                .setLocale(locale)
+                .setLevel(level)
+                .setType(type);
     }
 }
