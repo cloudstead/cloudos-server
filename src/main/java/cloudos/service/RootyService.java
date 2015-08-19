39292f5d5bf97683b8a1bee87f5e36d4806d2ab3
jonathan <jonathan@kyuss.org>
2015-08-19 04:34:27 -0700

    remove obsolete memcachedKey. move rooty request utility to rooty-tooty

--- src/main/java/cloudos/service/RootyService.java
+++ src/main/java/cloudos/service/RootyService.java
@@ -4,15 +4,9 @@ import cloudos.server.CloudOsConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import rooty.RootyHandler;
-import rooty.RootyMessage;
-import rooty.RootySender;
-import rooty.RootyStatusManager;
+import rooty.*;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.cobbzilla.util.daemon.ZillaRuntime.die;
-import static org.cobbzilla.util.system.Sleep.sleep;
 
 @Service @Slf4j
 public class RootyService {
@@ -27,20 +21,10 @@ public class RootyService {
 
     public <T extends RootyHandler> T getHandler(Class<T> clazz) { return configuration.getRooty().getHandler(clazz); }
 
-    public RootyMessage request(RootyMessage message) {
-        return request(message, TIMEOUT);
-    }
+    public RootyMessage request(RootyMessage message) { return request(message, TIMEOUT); }
 
     public RootyMessage request(RootyMessage message, long timeout) {
-        getSender().write(message);
-        sleep(250, "waiting for rooty to complete");
-        long start = System.currentTimeMillis();
-        while (System.currentTimeMillis() - start < timeout) {
-            final RootyMessage status = getStatusManager().getStatus(message.getUuid());
-            if (status != null && status.isFinished()) return status;
-            sleep(250, "waiting for rooty to complete");
-        }
-        return die("request timeout: " + message);
+        return RootyMain.request(message, getSender(), getStatusManager(), timeout);
     }
 
 }
