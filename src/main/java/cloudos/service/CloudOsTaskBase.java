1d1a2db90b39b55fb7b7d27d1de10e3c1be85d0e
jonathan <jonathan@kyuss.org>
2015-08-25 19:14:12 -0700

    better ssl cert validation

--- src/main/java/cloudos/service/CloudOsTaskBase.java
+++ src/main/java/cloudos/service/CloudOsTaskBase.java
@@ -2,6 +2,6 @@ package cloudos.service;
 
 import cloudos.service.task.CloudOsTaskResult;
 import org.cobbzilla.wizard.task.TaskBase;
+import org.cobbzilla.wizard.task.TaskEvent;
 
-public abstract class CloudOsTaskBase extends TaskBase<CloudOsTaskResult> {
-}
+public abstract class CloudOsTaskBase extends TaskBase<CloudOsTaskResult, TaskEvent> {}
--- src/main/java/cloudos/service/task/CloudOsTaskResult.java
+++ src/main/java/cloudos/service/task/CloudOsTaskResult.java
@@ -2,12 +2,13 @@ package cloudos.service.task;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.cobbzilla.wizard.task.TaskEvent;
 import org.cobbzilla.wizard.task.TaskResult;
 import rooty.RootyMessage;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
-public class CloudOsTaskResult extends TaskResult {
+public class CloudOsTaskResult extends TaskResult<TaskEvent> {
 
     @Getter @Setter
     private String rootyUuid;
